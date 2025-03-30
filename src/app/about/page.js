4274b2b21699db8d4144d5f2b39e50176d731544
Arnav Agarwal <arnav22agarwal@gmail.com>
2025-03-30 16:22:02 +0530

    safepoint
    everythign works until here lmao

---
+++ src/app/about/page.js
@@ -0,0 +1,13 @@
+import ImageBackground from '@/components/background'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+        <ImageBackground/>
+        
+    </div>
+  )
+}
+
+export default page
